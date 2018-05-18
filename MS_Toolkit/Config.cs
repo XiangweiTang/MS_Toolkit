3846c92bb498366257195f9657abb16d1b14c910
Xiangwei Tang <tangxiangwei@gmail.com>
2018-05-18 15:00:35 +0800

    Update

--- MS_Toolkit/Config.cs
+++ MS_Toolkit/Config.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace MS_Toolkit
 {
     class Config
     {
+        string TaskName = string.Empty;
+        public void LoadConfig(string configPath)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(configPath);
+            TaskName = xDoc["Root"].Attributes["TaskName"].Value;
+            var taskNode = xDoc[TaskName];
+            LoadTask(taskNode);            
+        }
 
+        protected virtual void LoadTask(XmlNode taskNode)
+        {
+            //Do nothing.
+        }
     }
     
     class ConfigExtractTextGrid:Config
